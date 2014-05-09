3a9ce719b8e20d495d2a4ffbc9a6aae0c0cc6a69
Christian Dullweber <xchrdw@gmail.com>
2014-05-09 17:43:36 +0200

    use wikidata apis for autocomplete and terms

--- app/controllers/PropertyController.php
+++ app/controllers/PropertyController.php
@@ -1,34 +1,39 @@
 <?php
+
 class PropertyController extends BaseController {
 
-  public function getItemSuggestions() {
-    $id = 0;
-    if (Input::has('id')) $id = Input::get('id');
+	public function getItemSuggestions() {
+		$id = 0;
+		if ( Input::has( 'id' ) ) $id = Input::get( 'id' );
 
-    $ids = DB::table('wbs_item_suggestionst')
-      ->select(DB::raw("'Q'+item_id as qid"))
-      ->where('property_id', $id)
-      ->get();
-    return $ids;
-  }
+		if ( starts_with( $id, 'P' ) ) {
+			$id = substr( $id, 1 );
+		}
 
-  public function getAddWatchTask() {
-    $item = 0;
-    $property = 0;
-    if (Input::has('item')) $item = Input::get('item');
-    if (Input::has('property')) $property = Input::get('property');
+		$ids = DB::table( 'wbs_item_suggestions' )
+			->select( DB::raw( "CONCAT('Q', item_id) as item_id" ) )
+			->where( 'property_id', $id )
+			->get();
+		return $ids;
+	}
 
-    if ($item && $property) {
-      $task = NULL;
-      if ($existingTask = WbsTask::where('item_id', $item)->where('property_id', $property)->first()) {
-        $task = $existingTask;
-      } else {
-        $task = new WbsTask();
-        $task->item_id = $item;
-        $task->property_id = $property;
-      }
-      $task->touch();
-      $task->save();
-    }
-  }
+	public function getAddWatchTask() {
+		$item = 0;
+		$property = 0;
+		if ( Input::has( 'item' ) ) $item = Input::get( 'item' );
+		if ( Input::has( 'property' ) ) $property = Input::get( 'property' );
+
+		if ( $item && $property ) {
+			$task = NULL;
+			if ( $existingTask = WbsTask::where( 'item_id', $item )->where( 'property_id', $property )->first() ) {
+				$task = $existingTask;
+			} else {
+				$task = new WbsTask();
+				$task->item_id = $item;
+				$task->property_id = $property;
+			}
+			$task->touch();
+			$task->save();
+		}
+	}
 }
---
+++ public/main.js
@@ -0,0 +1,105 @@
+$(document).ready(function() {
+	var properties = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		remote: './api/properties?q=%QUERY',
+		limit: 7
+	});
+	var query = {};
+
+	var api = 'http://www.wikidata.org/w/api.php';
+	var language = 'en';
+
+	properties.initialize();
+
+	$('#searchbox').typeahead(null, {
+		name: 'properties',
+		displayKey: function(property) {
+			return property['label'];
+		},
+		templates: {
+			suggestion: function(property) {
+				var ret = '<b>' + property.label + '</b>';
+				if (property.description) {
+					ret += '<br />' + property.description;
+				}
+				if (property.aliases) {
+					ret += '<br /><i>Also known as: ' + property.aliases + '</i>';
+				}
+				return ret;
+			}
+		},
+		source: function(query, process) {
+			$.ajax({
+				url: api,
+				dataType: 'jsonp',
+				success: function(data) { process(data.search); },
+				data: {'action': 'wbsearchentities', 'format':'json',
+					   'language': language, 'search': query, 'type': 'property'}
+			})
+		}
+	}).bind("typeahead:selected", function(evt, prop) {
+		getResults(prop['id'], true);
+	});
+
+	var getResults = function(propertyId, addToHistory) {
+
+		$('.resultcontainer').html('<img src="img/ajax-loader.gif" /> Please wait while the data is being loaded...');
+		$.ajax('./api/itemSuggestions?id=' + propertyId).done(function(data) {
+			ids = [];
+			$.each(data, function(k, v) {
+				ids.push( v.item_id);
+			});
+			ids = ids.slice(0,50);
+			$.ajax({
+				url: api,
+				dataType: 'jsonp',
+				data: {'action': 'wbgetentities', 'format':'json',
+					'ids': ids.join('|'), 'props': 'labels|descriptions|aliases', 'languages': language},
+				success: function(data) {
+					$resultcontainer = $('.resultcontainer');
+					$resultcontainer.text('');
+					var ret = '';
+					entities = data.entities;
+					$.each(ids, function(k, id) {
+						label = entities[id].labels ? entities[id].labels[language].value : id;
+						ret += '<p class="result"><a href="http://wikidata.org/wiki/' + id + '" target="_blank" data-item="' + id + '" data-property="' + propertyId + '"><strong>' + label + '</strong></a>';
+						if (entities[id].descriptions) {
+							ret += '<br />' + entities[id].descriptions[language].value;
+						}
+						ret += '</p>';
+					});
+					$resultcontainer.html(ret);
+					$('.resultcontainer a').click(function() {
+						$.ajax('./api/addWatchTask?item=' + $(this).data('item') + '&property=' + $(this).data('property')).done(function(data) {
+						});
+					});
+				}
+			});
+		});
+	};
+	$('#refresh').click(function() {
+		console.log('query', query);
+		loadPage();
+	});
+
+	location.search.substr(1).split("&").forEach(function(item) {query[item.split("=")[0]] = item.split("=")[1]});
+	var loadPage = function() {
+		if (query.hasOwnProperty('id')) {
+			id = 'P' + query['id'];
+
+			$.ajax({
+				url: api,
+				dataType: 'jsonp',
+				data: {'action': 'wbgetentities', 'format':'json',
+					'ids': id, 'props': 'labels', 'languages': language},
+				success: function(data) {
+					label = data.entities[id].labels[language].value;
+					$('#searchbox').val(label);
+				}
+			});
+			getResults(query.id, false);
+		}
+	};
+	loadPage();
+});
